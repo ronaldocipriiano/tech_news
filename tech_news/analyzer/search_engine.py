796eec24c0d9360edaaae7b795c2207c55f53360
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-04-23 19:55:44 -0300

    Feat: Implementando as funções do arquivo search_engine

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,42 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    title = title.lower()
+    result = []
+
+    for news in search_news({}):
+        if title in news["title"].lower():
+            result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        formatted_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    result = []
+
+    for news in search_news({"timestamp": formatted_date}):
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    category = category.lower()
+    result = []
+
+    for news in search_news(
+        {"category": {"$regex": f".*{category}.*", "$options": "i"}}
+    ):
+        result.append((news["title"], news["url"]))
+
+    return result
