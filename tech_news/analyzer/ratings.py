9089fb131ea461b1ba6679ad65c3537848635b6a
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-04-23 20:14:21 -0300

    Feat: Criando a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,19 @@
+from collections import Counter
+from tech_news.database import search_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    categories = [news["category"] for news in search_news({})]
+
+    if not categories:
+        return []
+
+    categories_count = Counter(categories)
+
+    sorted_categories = sorted(
+        categories_count.items(),
+        key=lambda category: (-category[1], category[0])
+    )
+
+    return [category[0] for category in sorted_categories[:5]]
